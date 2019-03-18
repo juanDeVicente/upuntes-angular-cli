5bd74a7d405a3ec76860daaccda957a2015a1ead
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-03-18 12:42:32 +0100

    Añadidos las plantillas para el contenido de todas las paginas de upuntes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
